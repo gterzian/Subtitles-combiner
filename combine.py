56adaef97cfcebe9ad5d7e98f0e481746f5a8f3c
Gregory <gregory.terzian@gmail.com>
2013-10-11 23:20:02 +0200

    more flexble args

--- combine.py
+++ combine.py
@@ -1,5 +1,15 @@
 
 from collections import OrderedDict
+import argparse
+
+
+def process_filenames(filenames_list):
+    for name in filenames_list:
+        yield name
+        
+def read_file(names):
+    for name in names:
+        yield read_subs(name)
 
 def read_subs(sub_file):
     with open(sub_file, 'rt') as f: 
@@ -7,37 +17,36 @@ def read_subs(sub_file):
             if line.strip():
                 yield line.strip().decode('utf-8')
                 
-def combine_in_dict(pinyin=None, traditional=None):
+def combine_in_dict(*args):
     subs = OrderedDict()
     line_number = None
-    for lines in zip(pinyin, traditional):
+    for lines in zip(*args):
         if lines[0].isdigit():
             subs[lines[0]] = {}
             line_number = lines[0]
         elif '-->' in lines[0]:
             subs[line_number]['time'] = lines[0]
         else:
-            subs[line_number]['pinyin'] = lines[0]
-            subs[line_number]['traditional'] = lines[1]
+            subs[line_number]['lines'] = [ line for line in lines]
     return subs
-                
-            
-if __name__ == '__main__':
-    print 'reading'
-    traditional = read_subs('Crouching Tiger Hidden Dragon.srt')
-    pinyin = read_subs('Crouching_Tiger_Hidden_Dragon.Pinyin.srt')
-    subs = combine_in_dict(pinyin=pinyin, traditional=traditional) 
-    with open('combined.srt', 'w') as f:
-        for item in subs.items():
-            f.write(item[0])
+
+parser = argparse.ArgumentParser(description='Process some subtitles.')
+parser.add_argument('files', type=str, metavar='Names', nargs='+', 
+                       help='the name of the subtitle files, without comma separation')
+args = parser.parse_args()
+file_names = process_filenames(args.files)
+subs = read_file(file_names)
+subs = combine_in_dict(*subs) 
+with open('combined.srt', 'w') as f:
+    for item in subs.items():
+        f.write(item[0])
+        f.write('\n')
+        f.write(item[1]['time'].encode('utf-8'))
+        f.write('\n')
+        for line in item[1]['lines']:
+            f.write(line.encode('utf-8'))
             f.write('\n')
-            f.write(item[1]['time'].encode('utf-8'))
-            f.write('\n')
-            f.write(item[1]['traditional'].encode('utf-8'))
-            f.write('\n')
-            f.write(item[1]['pinyin'].encode('utf-8'))
-            f.write('\n')
-            f.write('\n')
+        f.write('\n')
             
         
    
